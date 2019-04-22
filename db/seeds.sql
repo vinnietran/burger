7a172675f888e5239a289650ee1a2347611031e8
VinTran <tranquillo.vinnie@gmail.com>
2019-04-22 18:17:22 -0400

    Initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
