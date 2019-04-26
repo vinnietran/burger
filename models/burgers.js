b74dda7c1d46cf4e12bd7695b9342de07790c442
VinTran <tranquillo.vinnie@gmail.com>
2019-04-26 10:12:49 -0400

    Displaying Exsisting on index now

--- models/burgers.js
+++ models/burgers.js
@@ -5,16 +5,17 @@ var burger = {
     orm.all("burgers", function(res) {
       cb(res);
     });
-  }
+  },
 
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
 
 }
 
-
-
-
-
-
+// console.log(burger);
 
 
 module.exports = burger;
