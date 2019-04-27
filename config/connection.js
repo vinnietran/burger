5d93eb1e802d50e9e0e3fb5696779586d7214509
VinTran <tranquillo.vinnie@gmail.com>
2019-04-27 08:26:28 -0400

    Heroku Issues

--- config/connection.js
+++ config/connection.js
@@ -15,22 +15,22 @@ if (process.env.JAWSDB_URL) {
 	})
 };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "farmboy1",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "farmboy1",
+//   database: "burgers_db"
+// });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
